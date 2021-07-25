be391d55e69f21a2bdec573acb5d7c087aeeef25
paccionesawyer <57788768+paccionesawyer@users.noreply.github.com>
2021-07-25 00:21:53 -0400

    Add files via upload

--- boot.py
+++ boot.py
@@ -1,21 +1,25 @@
-# Complete project details at https://RandomNerdTutorials.com
-
-try:
-    import usocket as socket
-except:
-    import socket
-
-from machine import Pin
+import time
+from umqtt.simple import MQTTClient
+import ubinascii
+import machine
+import micropython
 import network
-
 import esp
 esp.osdebug(None)
-
 import gc
 gc.collect()
 
-ssid = 'Tufts_Wireless'
-password = ''
+ssid = 'WIFI NAME'
+password = 'WIFI PASSWORD'
+mqtt_server = 'IP ADDRESS OF BROKER' # Mywifi
+#EXAMPLE IP ADDRESS
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'hello'
+
+last_message = 0
+message_interval = 5
+counter = 0
 
 station = network.WLAN(network.STA_IF)
 
@@ -25,8 +29,5 @@ station.connect(ssid, password)
 while station.isconnected() == False:
     pass
 
-
 print('Connection successful')
 print(station.ifconfig())
-
-led = Pin(2, Pin.OUT)
